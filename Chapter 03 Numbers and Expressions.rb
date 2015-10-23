b81bb12631daec6b04cbafea6e5ca37b325d56cc
Amr Alaa <n1amr1@gmail.com>
2015-10-24 01:26:42 +0200

    Ranges

--- Chapter 03 Numbers and Expressions.rb
+++ Chapter 03 Numbers and Expressions.rb
@@ -335,3 +335,15 @@ puts now.yday
 puts now.zone
 puts now.utc?
 
+##############################################
+#Ranges
+
+puts (1..5).to_a.inspect
+puts "R is within A to Z!" if ('A'..'Z').include?('R')
+
+a = [2, 4, 6, 8, 10, 12]
+puts a[1..3].inspect
+
+a[1..3] = ["a", "b", "c"]
+puts a.inspect
+
