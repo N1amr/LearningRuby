073865654be0c56c47fe64ae30c78882b35df897
Amr Alaa <n1amr1@gmail.com>
2015-10-23 19:26:15 +0200

    First Code

---
+++ First Code.rb
@@ -0,0 +1,20 @@
+class Person
+  attr_accessor :name, :age, :gender
+end
+
+person = Person.new;
+person.name = "Amr Alaa";
+person.age = 21;
+person.gender = "Male";
+
+print "person is named "
+puts person.name
+puts prson.to_html()
+
+class Pet
+  attr_accessor :name, :age, :gender, :color
+end
+
+class Cat < Pet
+
+end
