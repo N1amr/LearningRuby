9a4c780074dcb32c4e01ccd7e3dd62806080450f
Amr Alaa <n1amr1@gmail.com>
2015-10-24 01:32:43 +0200

    Symbols

--- Chapter 03 Numbers and Expressions.rb
+++ Chapter 03 Numbers and Expressions.rb
@@ -347,3 +347,13 @@ puts a[1..3].inspect
 a[1..3] = ["a", "b", "c"]
 puts a.inspect
 
+##############################################
+#Symbols
+
+current_situation = "good"
+puts "Everything is fine" if current_situation == "good"
+puts "PANIC!" if current_situation == "bad"
+
+person1 = { :name => "Fred", :age => 20, :gender => :male }
+puts person1[:name]
+
