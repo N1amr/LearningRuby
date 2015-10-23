919aad2e9619efe9760bbf868bf1aa2e853dcdf5
Amr Alaa <n1amr1@gmail.com>
2015-10-23 21:23:42 +0200

    comparison

--- Chapter 03 Numbers and Expressions.rb
+++ Chapter 03 Numbers and Expressions.rb
@@ -1,3 +1,12 @@
 age = 21
 puts "you are a teenager" if age > 12 && age < 20
 puts "you are not a teenager" unless age > 12 && age < 20
+puts "you are on your twenties" if age.between?(20,29)
+
+x = 5
+y = 2
+puts x <=> y #Returns 0 if x and y are equal, 1 if x is higher, -1 if y is higher.
+
+5.times do
+  puts "test"
+end
