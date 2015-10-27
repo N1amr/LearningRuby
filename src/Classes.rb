68b7ad25138cb75543575220a3a8220612a70523
Amr Alaa <n1amr1@gmail.com>
2015-10-27 22:34:42 +0200

    Nested Classes + fixes

--- src/Classes.rb
+++ src/Classes.rb
@@ -1,19 +1,22 @@
 class Computer
-  $manufacturer = "Mango Computer, Inc." #global variable
-  @@files = "Hello, world!" #class variable
+    #global variable
+    $manufacturer = "Mango Computer, Inc."
+    #class variable
+    @@files = "Hello, world!"
+    def initialize(username, password)
+        #instance variable
+        @username = username
+        @password = password
+    end
 
-  def initialize(username, password)
-    @username = username #instance variable
-    @password = password
-  end
+    def current_user
+        @username
+    end
 
-  def current_user
-    @username
-  end
-
-  def self.display_files # or   def Computer.display_files
-    @@files
-  end
+    # or   def Computer.display_files
+    def self.display_files
+        @@files
+    end
 end
 
 # Make a new Computer instance:
@@ -27,37 +30,59 @@ puts "Manufacturer: #{$manufacturer}"
 
 puts "Files: #{Computer.display_files}"
 # @@files belongs to the Computer class.
-
 #Inheritance
+class BaseClass
+end
+
 class DerivedClass < BaseClass
 
 end
 
 #You can directly access the attributes or methods of a superclass with Ruby's built-in super keyword
-class DerivedClass < Base
-  def some_method
-    super(optional args)
+class DerivedClass < BaseClass
+    def some_method
+        super(optional args)
     # Some stuff
-  end
+    end
 end
 
 class Person
-  def initialize(name, job)
-    @name = name
-    @job = job
-  end
+    def initialize(name, job)
+        @name = name
+        @job = job
+    end
 
-  #  attr_reader :name
-  def name
-    @name
-  end
+    #  attr_reader :name
+    def name
+        @name
+    end
 
-  #  attr_writer :name
-  def job=(new_job)
-    @job = new_job
-  end
+    #  attr_writer :name
+    def job=(new_job)
+        @job = new_job
+    end
 
-  #  attr_accessor :job
+#  attr_accessor :job
 
 end
+
+class Drawing
+    def self.make_circle
+        Circle.new
+    end
+
+    class Line
+    end
+
+    class Circle
+        def msg
+            puts "class Circle"
+        end
+    end
+end
+
+a = Drawing.make_circle
+a.msg
+a = Drawing::Circle.new
+a.msg
 
